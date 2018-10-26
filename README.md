ddefb08586f421cf2a81e71ffd72a2a87f1ecadc
Wei Lin <Wei1234c@gmail.com>
2018-10-26 23:13:50 +0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+### Digital Signal Processing
+
+Practices bases on "Digital Signal Processing" on Coursera.
+https://www.coursera.org/learn/dsp/home/welcome
---
+++ codes/MP3_player/assignment1.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+
+def weighted(x):
+    N = len(x)
+    n = np.arange(N)
+    w = (1 - np.cos(2 * np.pi * n / (N - 1)))
+    w = w / np.dot(w, w)
+    return x * w
+
+
+def dft(samples):
+    samples = np.asarray(samples).reshape((-1, 1))
+    N = len(samples)
+    k = n = np.arange(N)
+    W = np.exp(np.outer(-1j * np.pi * 2 / N * k, n))
+    spectrum = np.dot(W, samples).ravel()
+    return spectrum
+
+
+def get_db(x):
+    db = 20 * np.log10(x)
+    db[np.where(x == 0)[0]] = -100
+    return db
+
+
+def scaled_fft_db(x, targeted_db = 96):
+    x = weighted(x)
+    spectrum = abs(dft(x)) / len(x)
+    spectrum = get_db(spectrum)
+    spectrum = spectrum + targeted_db - np.max(spectrum)
+    return spectrum[:257]
