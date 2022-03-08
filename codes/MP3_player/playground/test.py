c1967029a2c01f5e2dfa231249fefce301f58acd
wei_lin <wei1234c@gmail.com>
2022-03-09 00:19:14 +0800

    resync.

--- codes/MP3_player/playground/test.py
+++ codes/MP3_player/playground/test.py
@@ -1,6 +1,6 @@
 import check_assignment as ca
-# ca.check_assignment2(plot = True)
-ca.check_assignment3()
+ca.check_assignment2(plot = True)
+# ca.check_assignment3()
 # ca.check_assignment4()
 
 # import generate_submission
